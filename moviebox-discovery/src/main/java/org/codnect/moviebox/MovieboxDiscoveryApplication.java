30fca36cf72d51b276671dfe62f9dc2beec49909
Burak Koken <burakkokenn@gmail.com>
2020-02-01 20:17:52 +0200

    Added the moviebox discovery module

---
+++ moviebox-discovery/src/main/java/org/codnect/moviebox/MovieboxDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package org.codnect.moviebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieboxDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieboxDiscoveryApplication.class, args);
+    }
+
+}
