f129b6d07103c2ae346bba86bd0d5cae6566cf70
Burak Koken <burakkokenn@gmail.com>
2020-02-01 19:03:28 +0200

    Added the models for movie-service and their associations.

---
+++ moviebox-movie/src/main/java/org/codnect/moviebox/model/Genre.java
@@ -0,0 +1,77 @@
+package org.codnect.moviebox.model;
+
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "Genres")
+public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NaturalId
+    @Column(name = "genre_name")
+    private String name;
+
+    @ManyToMany(mappedBy = "genres", fetch = FetchType.LAZY)
+    private Set<Movie> movies = new HashSet<>();
+
+    @ManyToMany(mappedBy = "genres", fetch = FetchType.LAZY)
+    private Set<TvSeries> tvSeries = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(Set<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public Set<TvSeries> getTvSeries() {
+        return tvSeries;
+    }
+
+    public void setTvSeries(Set<TvSeries> tvSeries) {
+        this.tvSeries = tvSeries;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) {
+            return true;
+        }
+        if(obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Genre genre = (Genre) obj;
+        return Objects.equals(name, genre.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+}
---
+++ moviebox-movie/src/main/java/org/codnect/moviebox/model/TvSeries.java
@@ -0,0 +1,120 @@
+package org.codnect.moviebox.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "TvSeries")
+public class TvSeries {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "tv_series_name")
+    private String name;
+
+    @Lob
+    @Column(name = "tv_series_overview")
+    private String overview;
+
+    @ManyToMany(
+            fetch = FetchType.LAZY,
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE}
+    )
+    @JoinTable(
+            name = "TvSeries_Genre",
+            joinColumns = { @JoinColumn(name = "tv_series_id") },
+            inverseJoinColumns = { @JoinColumn(name = "genre_id") }
+    )
+    private Set<Genre> genres = new HashSet<>();
+
+    @OneToMany(
+            mappedBy = "tvSeries",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private Set<Season> seasons;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public Set<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(Set<Genre> genres) {
+        this.genres = genres;
+    }
+
+    public Set<Season> getSeasons() {
+        return seasons;
+    }
+
+    public void setSeasons(Set<Season> seasons) {
+        this.seasons = seasons;
+    }
+
+    public void addGenre(Genre genre) {
+        genres.add(genre);
+        genre.getTvSeries().add(this);
+    }
+
+    public void removeGenre(Genre genre) {
+        genres.remove(genre);
+        genre.getTvSeries().remove(this);
+    }
+
+    public void addSeason(Season season) {
+        seasons.add(season);
+        season.setTvSeries(this);
+    }
+
+    public void removeSeason(Season season) {
+        seasons.remove(season);
+        season.setTvSeries(null);
+    }
+
+    public boolean equals(Object obj) {
+        if(this == obj) {
+            return true;
+        }
+        if(obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        TvSeries tvSeries = (TvSeries) obj;
+        if(tvSeries.id == null || id == null) {
+            return false;
+        }
+        return Objects.equals(id, tvSeries.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+}
